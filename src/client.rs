27507ad23fff106051c9f557b74b3c75400e9750
Alex Pikalov <alex.pikalov.khar@gmail.com>
2016-12-12 22:32:49 +0100

    Create CRDS custom Error and Result

--- src/client.rs
+++ src/client.rs
@@ -2,15 +2,17 @@ use std::net;
 use std::io;
 use std::io::Write;
 
-use super::consistency::Consistency;
-use super::frame::{Frame, Opcode};
-use super::IntoBytes;
-use super::frame::parser::parse_frame;
+use consistency::Consistency;
+use frame::{Frame, Opcode};
+use IntoBytes;
+use frame::parser::parse_frame;
 use types::*;
 use types::value::*;
+
 use frame::frame_query::*;
 use compression::Compression;
 use authenticators::Authenticator;
+use error;
 
 #[derive(Clone, Debug)]
 pub struct Credentials {
@@ -25,16 +27,17 @@ pub struct CDRS<T: Authenticator> {
 }
 
 impl<T: Authenticator> CDRS<T> {
-    pub fn new(addr: String, authenticator: T) -> io::Result<CDRS<T>> {
+    pub fn new(addr: String, authenticator: T) -> error::Result<CDRS<T>> {
         return net::TcpStream::connect(format!("{}:9042", addr).as_str())
             .map(|socket| CDRS {
                 tcp: socket,
                 compressor: Compression::None,
                 authenticator: authenticator
-            });
+            })
+            .map_err(|err| error::Error::Io(err));
     }
 
-    pub fn start(&mut self, compressor: Compression) -> io::Result<Frame> {
+    pub fn start(&mut self, compressor: Compression) -> error::Result<Frame> {
         self.compressor = compressor;
         let mut tcp = try!(self.tcp.try_clone());
         let startup_frame = Frame::new_req_startup(compressor.into_string()).into_cbytes();
@@ -58,39 +61,19 @@ impl<T: Authenticator> CDRS<T> {
 
                 return Ok(auth_response);
             } else {
-                return Err(io::Error::new(
+                let io_err = io::Error::new(
                     io::ErrorKind::NotFound,
                     format!("Unsupported type of authenticator. {:?} got, but {} is supported.",
                         authenticator,
-                        self.authenticator.get_cassandra_name())));
+                        self.authenticator.get_cassandra_name()));
+                return Err(error::Error::Io(io_err));
             }
-            // if authenticator.as_str() == "org.apache.cassandra.auth.PasswordAuthenticator" {
-            //     let mut tcp_auth = try!(self.tcp.try_clone());
-            //
-            //     let credentials = self.credentials.clone().ok_or(io::Error::new(
-            //         io::ErrorKind::NotFound,
-            //         "Credetials required, but not found"
-            //     ))?;
-            //
-            //     let mut token = vec![0];
-            //     token.extend_from_slice(credentials.username.into_bytes().as_slice());
-            //     token.push(0);
-            //     token.extend_from_slice(credentials.password.into_bytes().as_slice());
-            //     let auth_token_bytes = CBytes::new(token).into_cbytes();
-            //
-            //     try!(tcp_auth.write(Frame::new_req_auth_response(auth_token_bytes).into_cbytes().as_slice()));
-            //     let auth_response = try!(parse_frame(tcp_auth, &compressor));
-            //
-            //     return Ok(auth_response);
-            // } else {
-            //
-            // }
         }
 
         unimplemented!();
     }
 
-    pub fn options(&self) -> io::Result<Frame> {
+    pub fn options(&self) -> error::Result<Frame> {
         let mut tcp = try!(self.tcp.try_clone());
         let options_frame = Frame::new_req_options().into_cbytes();
 
@@ -98,7 +81,7 @@ impl<T: Authenticator> CDRS<T> {
         return parse_frame(tcp, &self.compressor);
     }
 
-    pub fn prepare(&self, query: String) -> io::Result<Frame> {
+    pub fn prepare(&self, query: String) -> error::Result<Frame> {
         let mut tcp = try!(self.tcp.try_clone());
         let options_frame = Frame::new_req_prepare(query).into_cbytes();
 
@@ -106,7 +89,7 @@ impl<T: Authenticator> CDRS<T> {
         return parse_frame(tcp, &self.compressor);
     }
 
-    pub fn execute(&self, id: CBytesShort, query_parameters: ParamsReqQuery) -> io::Result<Frame> {
+    pub fn execute(&self, id: CBytesShort, query_parameters: ParamsReqQuery) -> error::Result<Frame> {
         let mut tcp = try!(self.tcp.try_clone());
         let options_frame = Frame::new_req_execute(id, query_parameters).into_cbytes();
 
@@ -122,7 +105,7 @@ impl<T: Authenticator> CDRS<T> {
             page_size: Option<i32>,
             paging_state: Option<CBytes>,
             serial_consistency: Option<Consistency>,
-            timestamp: Option<i64>) -> io::Result<Frame> {
+            timestamp: Option<i64>) -> error::Result<Frame> {
 
         let mut tcp = try!(self.tcp.try_clone());
         let query_frame = Frame::new_req_query(query.clone(),
