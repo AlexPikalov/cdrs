f402cdd4ccb87551c59ee7d0670f67aeb8e2fb1e
Alex Pikalov <alex.pikalov.khar@gmail.com>
2017-02-01 20:14:09 +0100

    Create registerframe

--- src/frame/frame_register.rs
+++ src/frame/frame_register.rs
@@ -20,27 +20,26 @@ impl IntoBytes for BodyReqRegister {
     }
 }
 
-// Frame implementation related to BodyReqStartup
+// Frame implementation related to BodyReqRegister
 
-// impl Frame {
-//     /// Creates new frame of type `options`.
-//     pub fn new_req_options() -> Frame {
-//         let version = Version::Request;
-//         let flag = Flag::Ignore;
-//         // sync client
-//         let stream: u64 = 0;
-//         let opcode = Opcode::Options;
-//         let body = BodyReqOptions::new();
-//
-//         return Frame {
-//             version: version,
-//             flags: vec![flag],
-//             stream: stream,
-//             opcode: opcode,
-//             body: body.into_cbytes(),
-//             // for request frames it's always None
-//             tracing_id: None,
-//             warnings: vec![]
-//         };
-//     }
-// }
+impl Frame {
+    /// Creates new frame of type `REGISTER`.
+    pub fn new_req_register(register: BodyReqRegister) -> Frame {
+        let version = Version::Request;
+        let flag = Flag::Ignore;
+        // sync client
+        let stream: u64 = 0;
+        let opcode = Opcode::Register;
+
+        return Frame {
+            version: version,
+            flags: vec![flag],
+            stream: stream,
+            opcode: opcode,
+            body: register.into_cbytes(),
+            // for request frames it's always None
+            tracing_id: None,
+            warnings: vec![]
+        };
+    }
+}
