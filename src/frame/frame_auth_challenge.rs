b5e0f215e897cf727f573db7563bff4e0f0f2dae
Alex Pikalov <alex.pikalov.khar@gmail.com>
2017-02-06 23:16:06 +0100

    Add tests for fraem_auth_challenge

--- src/frame/frame_auth_challenge.rs
+++ src/frame/frame_auth_challenge.rs
@@ -15,3 +15,18 @@ impl FromCursor for BodyResAuthChallenge {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use FromCursor;
+    use std::io::Cursor;
+
+    #[test]
+    fn body_res_auth_challenge_from_cursor() {
+        let few_bytes: Vec<u8> = vec![0, 0, 0, 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let mut cursor = Cursor::new(few_bytes);
+        let body = BodyResAuthChallenge::from_cursor(&mut cursor);
+        assert_eq!(body.data.into_plain(), vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+}
