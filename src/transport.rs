f5639077834a502a96c29fe571d4743ff5c2cd99
harrydevnull <hareendran.c.nair@gmail.com>
2017-02-06 14:29:19 -0500

    trying to fit in

--- src/transport.rs
+++ src/transport.rs
@@ -2,6 +2,36 @@ use std::io;
 use std::io::{Read, Write};
 use std::net;
 use std::net::TcpStream;
+#[cfg(feature = "ssl")]
+use openssl::ssl::{SslStream, SslConnector,HandshakeError};
+
+pub  trait  CDRSTransport{
+
+    fn try_clone(&self) -> io::Result<Self>;
+    fn close(&mut self, close: net::Shutdown) -> io::Result<()>;
+}
+
+impl CDRSTransport  for Transport {
+
+    /// In opposite to `TcpStream`'s `try_clone` this method
+    /// creates absolutely new connection - it gets an address
+    /// of a peer from `Transport` and creates a new encrypted
+    /// transport with new TCP stream under hood.
+    fn try_clone(&self) -> io::Result<Transport> {
+        let addr = try!( self.tcp.peer_addr());
+
+        net::TcpStream::connect(addr)
+        .map( | socket | Transport {
+        tcp: socket
+        })
+    }
+
+     fn close(&mut self, close: net::Shutdown) -> io::Result<()> {
+            return self.tcp.shutdown(close);
+    }
+
+
+}
 
 #[derive(Debug)]
 pub struct Transport {
@@ -16,22 +46,6 @@ impl Transport {
             });
     }
 
-    /// In opposite to `TcpStream`'s `try_clone` this method
-    /// creates absolutely new connection - it gets an address
-    /// of a peer from `Transport` and creates a new encrypted
-    /// transport with new TCP stream under hood.
-    pub fn try_clone(&self) -> io::Result<Transport> {
-        let addr = try!(self.tcp.peer_addr());
-
-        net::TcpStream::connect(addr)
-            .map(|socket| Transport {
-                tcp: socket
-            })
-    }
-
-    pub fn close(&mut self, close: net::Shutdown) -> io::Result<()> {
-        return self.tcp.shutdown(close);
-    }
 }
 
 impl Read for Transport {
@@ -47,5 +61,83 @@ impl Write for Transport {
 
     fn flush(&mut self) -> io::Result<()> {
         return self.tcp.flush();
+    }
+}
+/*************************************/
+/** TLS**/
+/**************************************/
+#[cfg(feature = "ssl")]
+impl CDRSTransport for TransportTls{
+
+    /// In opposite to `TcpStream`'s `try_clone` this method
+    /// creates absolutely new connection - it gets an address
+    /// of a peer from `TransportTls` and creates a new encrypted
+    /// connection with a new TCP stream under hood.
+     fn try_clone(&self) -> io::Result<TransportTls> {
+        let addr = try!(self.ssl.get_ref().peer_addr());
+        let ip_string = format!("{}", addr.ip());
+
+        let res =  net::TcpStream::connect(addr)
+            .map(|socket|
+                     self.connector.connect(ip_string.as_str(), socket)
+                         .map(|sslsocket| TransportTls {ssl: sslsocket, connector: self.connector.clone()}));
+
+        res.and_then(|res| {
+            res.map(|n: TransportTls| n ).map_err(|e| io::Error::new(io::ErrorKind::Other,e))
+        })
+    }
+
+     fn close(&mut self, _close: net::Shutdown) -> io::Result<()> {
+        return self.ssl.shutdown().map_err(|e| io::Error::new(io::ErrorKind::Other,e)).and_then(|_| Ok(()));
+    }
+}
+#[cfg(feature = "ssl")]
+pub struct TransportTls {
+    ssl: SslStream<TcpStream>,
+    connector: SslConnector
+}
+#[cfg(feature = "ssl")]
+impl TransportTls {
+
+    pub fn new(addr: &str, connector: & SslConnector) -> io::Result<TransportTls> {
+        let a: Vec<&str> = addr.split(':').collect();
+        let res =  net::TcpStream::connect(addr)
+            .map(|socket|
+                     connector.connect(a[0], socket)
+                         .map(|sslsocket|
+                                  TransportTls {
+                                      ssl: sslsocket,
+                                      connector: connector.clone()
+                                  }
+                         )
+            );
+
+        res.and_then(|res| {
+            // transform `Ok(Result<TransportTls,HandshakeError<net::TcpStream>>)` into `Ok(Result<TransportTls, io::Error>)`
+            res
+                // transform n to TransportTls
+                .map(|n: TransportTls| n )
+                // transform `HandshakeError` into `'io::Error`
+                .map_err(|e| io::Error::new(io::ErrorKind::Other,e))
+
+        })
+    }
+
+
+}
+#[cfg(feature = "ssl")]
+impl Read for TransportTls {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        return self.ssl.read(buf);
+    }
+}
+#[cfg(feature = "ssl")]
+impl Write for TransportTls {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        return self.ssl.write(buf);
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        return self.ssl.flush();
     }
 }
