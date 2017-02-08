07bf6b868e65e44af0b92870f58b6a134271efed
harrydevnull <hareendran.c.nair@gmail.com>
2017-02-08 10:02:13 -0500

    formated with rustfmt

--- src/frame/frame_auth_response.rs
+++ src/frame/frame_auth_response.rs
@@ -52,22 +52,23 @@ mod tests {
     #[test]
     fn body_req_auth_response() {
         let few_bytes: Vec<u8> = vec![0, 0, 0, 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-        let  data = CBytes::new(few_bytes);
+        let data = CBytes::new(few_bytes);
         let body = BodyReqAuthResponse::new(data);
-        assert_eq!(body.data.into_plain(), vec![0, 0, 0, 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        assert_eq!(body.data.into_plain(),
+                   vec![0, 0, 0, 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 
     #[test]
     fn body_req_auth_frame() {
         let few_bytes: Vec<u8> = vec![0, 0, 0, 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
         let frame = Frame::new_req_auth_response(few_bytes);
-        assert_eq!(frame.version,Version::Request);
-        assert_eq!(frame.flags,vec![Flag::Ignore]);
-        assert_eq!(frame.stream,0);
-        assert_eq!(frame.opcode,Opcode::AuthResponse);
-        assert_eq!(frame.tracing_id,None);
+        assert_eq!(frame.version, Version::Request);
+        assert_eq!(frame.flags, vec![Flag::Ignore]);
+        assert_eq!(frame.stream, 0);
+        assert_eq!(frame.opcode, Opcode::AuthResponse);
+        assert_eq!(frame.tracing_id, None);
         //assert_eq!(frame.warnings,vec![]);
-        assert_eq!(frame.body,vec![0, 0, 0, 14,0, 0, 0, 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        assert_eq!(frame.body,
+                   vec![0, 0, 0, 14, 0, 0, 0, 10, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     }
 }
-
