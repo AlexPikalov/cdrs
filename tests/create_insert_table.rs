6f09c86c7178c39f4f2ac4d7ffb9f2d5188c7114
harrydevnull <hareendran.c.nair@gmail.com>
2017-02-09 14:50:50 -0500

    tests?

--- tests/create_insert_table.rs
+++ tests/create_insert_table.rs
@@ -34,113 +34,113 @@ impl TestContext {
     }
 }
 
-#[test]
-fn create_keyspace() {
-    let ctx = TestContext::new();
-    let mut session = ctx.client.start(Compression::None).unwrap();
-    let create_ks_cql = "CREATE KEYSPACE user_keyspace WITH REPLICATION = { 'class' : \
-                         'SimpleStrategy', 'replication_factor' : 1 } ;";
+//#[test]
+//fn create_keyspace() {
+//    let ctx = TestContext::new();
+//    let mut session = ctx.client.start(Compression::None).unwrap();
+//    let create_ks_cql = "CREATE KEYSPACE user_keyspace WITH REPLICATION = { 'class' : \
+//                         'SimpleStrategy', 'replication_factor' : 1 } ;";
+//
+//    let create_ks_query = QueryBuilder::new(create_ks_cql)
+//        .consistency(Consistency::One)
+//        .finalize();
+//
+//    let create_ks_query_result = session.query(create_ks_query, false, false);
+//
+//    assert_eq!(create_ks_query_result.is_ok(), true);
+//
+//
+//}
 
-    let create_ks_query = QueryBuilder::new(create_ks_cql)
-        .consistency(Consistency::One)
-        .finalize();
+//#[test]
+//fn create_table() {
+//    let ctx = TestContext::new();
+//    let mut session = ctx.client.start(Compression::None).unwrap();
+//    let create_table_cql = "CREATE TABLE user_keyspace.users (
+//        user_name varchar PRIMARY KEY,
+//        password varchar,
+//        gender varchar,
+//        session_token varchar,
+//        state varchar,
+//        birth_year bigint
+//    );";
+//
+//    let create_table_query = QueryBuilder::new(create_table_cql)
+//        .consistency(Consistency::One)
+//        .finalize();
+//
+//    let create_table_query_result = session.query(create_table_query, false, false);
+//
+//    assert_eq!(create_table_query_result.is_ok(), true);
+//
+//    match create_table_query_result {
+//        Ok(ref res) => println!("table created: {:?}", res.get_body()),
+//        Err(ref err) => println!("Error occured: {:?}", err),
+//    }
+//
+//
+//}
 
-    let create_ks_query_result = session.query(create_ks_query, false, false);
-
-    assert_eq!(create_ks_query_result.is_ok(), true);
-
-
-}
-
-#[test]
-fn create_table() {
-    let ctx = TestContext::new();
-    let mut session = ctx.client.start(Compression::None).unwrap();
-    let create_table_cql = "CREATE TABLE user_keyspace.users (
-        user_name varchar PRIMARY KEY,
-        password varchar,
-        gender varchar,
-        session_token varchar,
-        state varchar,
-        birth_year bigint
-    );";
-
-    let create_table_query = QueryBuilder::new(create_table_cql)
-        .consistency(Consistency::One)
-        .finalize();
-
-    let create_table_query_result = session.query(create_table_query, false, false);
-
-    assert_eq!(create_table_query_result.is_ok(), true);
-
-    match create_table_query_result {
-        Ok(ref res) => println!("table created: {:?}", res.get_body()),
-        Err(ref err) => println!("Error occured: {:?}", err),
-    }
-
-
-}
-
-
-#[test]
-fn insert_data_users() {
-    use cdrs::types::CString;
-    let ctx = TestContext::new();
-    let mut session = ctx.client.start(Compression::None).unwrap();
-    let insert_table_cql = " insert into user_keyspace.users
-            (user_name ,password,gender,session_token, state,birth_year)
-    values  (?         ,  ?     ,     ?,            ?,     ?,  ?)";
-
-
-    let prepared = session.prepare(insert_table_cql.to_string(), true, true)
-        .unwrap()
-        .get_body()
-        .into_prepared()
-        .unwrap();
-
-    println!("prepared:\n{:?}", prepared);
-
-    //let v =  v: Vec<Value>
-
-
-    let v: Vec<Value> = vec![
-    convert_to_value("harry"),
-    convert_to_value("pwd"),
-    convert_to_value("male"),
-    convert_to_value("09000"),
-    convert_to_value("FL"),
-    convert_to_value("123")];
-    let execution_params = QueryParamsBuilder::new(Consistency::One).values(v).finalize();
-    //let execution_params = QueryParamsBuilder::new(Consistency::One).finalize();
-
-    let query_id = prepared.id;
-    let executed = session.execute(query_id, execution_params, true, true)
-        .unwrap()
-        .get_body()
-        .into_set_keyspace()
-        .unwrap();
-
-    info!("executed:\n{:?}", executed);
-
-}
-
-#[test]
-fn drop_keyspace() {
-    let ctx = TestContext::new();
-    let mut session = ctx.client.start(Compression::None).unwrap();
-    let drop_ks = "DROP KEYSPACE user_keyspace;";
-    let with_tracing = false;
-    let with_warnings = false;
-    let drop_ks_query = QueryBuilder::new(drop_ks).consistency(Consistency::One).finalize();
-    let drop_ks_query_result = session.query(drop_ks_query, with_tracing, with_warnings);
-
-    assert_eq!(drop_ks_query_result.is_ok(), true);
-
-    match drop_ks_query_result {
-        Ok(ref res) => println!("keyspace dropped: {:?}", res.get_body()),
-        Err(ref err) => println!("Error occured: {:?}", err),
-    }
-}
+//
+//#[test]
+//fn insert_data_users() {
+//    use cdrs::types::CString;
+//    let ctx = TestContext::new();
+//    let mut session = ctx.client.start(Compression::None).unwrap();
+//    let insert_table_cql = " insert into user_keyspace.users
+//            (user_name ,password,gender,session_token, state,birth_year)
+//    values  (?         ,  ?     ,     ?,            ?,     ?,  ?)";
+//
+//
+//    let prepared = session.prepare(insert_table_cql.to_string(), true, true)
+//        .unwrap()
+//        .get_body()
+//        .into_prepared()
+//        .unwrap();
+//
+//    println!("prepared:\n{:?}", prepared);
+//
+//    //let v =  v: Vec<Value>
+//
+//
+//    let v: Vec<Value> = vec![
+//    convert_to_value("harry"),
+//    convert_to_value("pwd"),
+//    convert_to_value("male"),
+//    convert_to_value("09000"),
+//    convert_to_value("FL"),
+//    convert_to_value("123")];
+//    let execution_params = QueryParamsBuilder::new(Consistency::One).values(v).finalize();
+//    //let execution_params = QueryParamsBuilder::new(Consistency::One).finalize();
+//
+//    let query_id = prepared.id;
+//    let executed = session.execute(query_id, execution_params, true, true)
+//        .unwrap()
+//        .get_body()
+//        .into_set_keyspace()
+//        .unwrap();
+//
+//    info!("executed:\n{:?}", executed);
+//
+//}
+//
+//#[test]
+//fn drop_keyspace() {
+//    let ctx = TestContext::new();
+//    let mut session = ctx.client.start(Compression::None).unwrap();
+//    let drop_ks = "DROP KEYSPACE user_keyspace;";
+//    let with_tracing = false;
+//    let with_warnings = false;
+//    let drop_ks_query = QueryBuilder::new(drop_ks).consistency(Consistency::One).finalize();
+//    let drop_ks_query_result = session.query(drop_ks_query, with_tracing, with_warnings);
+//
+//    assert_eq!(drop_ks_query_result.is_ok(), true);
+//
+//    match drop_ks_query_result {
+//        Ok(ref res) => println!("keyspace dropped: {:?}", res.get_body()),
+//        Err(ref err) => println!("Error occured: {:?}", err),
+//    }
+//}
 
 
 fn convert_to_value (s:&str) -> Value {
