95f5dff357ba8898d20baf5736fef798e9609709
Alex Pikalov <alex.pikalov.khar@gmail.com>
2018-01-03 22:59:45 +0100

    add different types of connection to cluster

--- examples/version_2.rs
+++ examples/version_2.rs
@@ -19,9 +19,9 @@ fn main() {
   let cluster = Cluster::new(vec![_ADDR]);
   let mut no_compression = cluster.connect(RoundRobin::new(), NoneAuthenticator {})
                                   .expect("No compression connection error");
-  let mut lz4_compression = cluster.connect(RoundRobin::new(), NoneAuthenticator {})
+  let mut lz4_compression = cluster.connect_lz4(RoundRobin::new(), NoneAuthenticator {})
                                    .expect("LZ4 compression connection error");
-  let mut snappy_compression = cluster.connect(RoundRobin::new(), NoneAuthenticator {})
+  let mut snappy_compression = cluster.connect_snappy(RoundRobin::new(), NoneAuthenticator {})
                                       .expect("Snappy compression connection error");
 
   create_keyspace(&mut no_compression);
